eba66c2a7d3fb9d8b93cfd706ae7faaf42746e38
ech1 <ech1@nowhere.com>
2020-08-03 13:36:37 +0100

    virtmanager

--- virt-manager/virtmanager.sh
+++ virt-manager/virtmanager.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+sudo apt install virt-manager qemu libvirt0 -y
+
+
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
+sudo systemctl status libvirtd
+
+
+#thats the base install, now the virtual routing
+
+sudo apt install ebtables dnsmasq bridge-utils -y
+
+#then open up virtmanager, and create ur qemu kvm as usual, but dont forget to create the NAT beforehand
+
